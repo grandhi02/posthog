3bbb1cd47c41c141255afce690fd63884975c2e7
Adam Bowker <adam.b@posthog.com>
2026-01-30 13:21:19 -0800

    fix(product tours): fix element text selection for element inference

--- frontend/src/toolbar/product-tours/productToursLogic.ts
+++ frontend/src/toolbar/product-tours/productToursLogic.ts
@@ -283,6 +283,7 @@ export const productToursLogic = kea<productToursLogicType>([
                 selectTour: () => null,
                 newTour: () => null,
                 removeStep: () => null,
+                startPreviewMode: () => null,
             },
         ],
         sidebarTransitioning: [
